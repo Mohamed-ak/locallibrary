d1b106622f145e848a39dfbc5a63265c7d1f59cc
mohamed <fofmak26@gmail.com>
2019-03-09 07:46:44 +0000

    Adds the home page

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
+from .models import Author, Book, BookInstance, Genre
 # Create your views here.
+
+def index(request):
+    """View function for the home page"""
+
+    # Generate the number of the main objects
+    num_books = Book.objects.count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact="a").count()
+    num_authors = Author.objects.all().count()
+    num_genres = Genre.objects.count()
+
+    context = {
+        'num_books': num_authors,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
+    }
+
+    return render(request,'index.html', context)
